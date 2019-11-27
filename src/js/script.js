aa646588805ba033a2104f3eaf687e9f219dd9fc
Jacek Kącki <jkacki@gmail.com>
2019-11-27 17:41:31 +0100

    correct mobile mode

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-irregular-whitespace */
 'use strict';
 
+/*Check width screen*/
+if(document.body.clientWidth < 768){
+  document.querySelector('.sidebar').classList.toggle('hidden');
+  document.querySelector('.main-content').classList.toggle('wide');  
+}
+
 /*Toggle button hamburger*/
 document.querySelector('.hamburger').addEventListener('click', function(event) {
   event.preventDefault();
